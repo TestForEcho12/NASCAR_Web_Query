18535a60860b9088615ccb81e25d591620695fbf
Greg <33642294+TestForEcho12@users.noreply.github.com>
2017-12-27 19:54:09 -0500

    Update and rename db.py to Database.py
    Quite a few updates to Database.py including live race position tracking

--- Database.py
+++ Database.py
@@ -17,28 +17,29 @@ class Database:
         c = conn.cursor()
         c.execute('CREATE TABLE IF NOT EXISTS Race_Results ('
                   'driver_id INTEGER,'			# __init__
-                  'race_id INTEGER,'				# __init__
-                  'qual INTEGER,'					# __init__
-                  'pole INTEGER,'       		   # __init__	
-                  'stage1 INTEGER,'           # update_results_DB
-                  'stage2 INTEGER,'           # update_results_DB
-                  'stage3 INTEGER,'           # update_results_DB
-                  'finish INTEGER,'           # update_results_DB
-                  'laps_led INTEGER,'			   # update_results_DB
-                  'win INTEGER,'              # update_results_DB
+                  'race_id INTEGER,'			# __init__
+                  'qual INTEGER,'				# __init__
+                  'pole INTEGER,'       		# __init__	
+                  'stage1 INTEGER,'             # update_results_DB
+                  'stage2 INTEGER,'             # update_results_DB
+                  'stage3 INTEGER,'             # update_results_DB
+                  'finish INTEGER,'             # update_results_DB
+                  'laps_led INTEGER,'		    # update_results_DB
+                  'win INTEGER,'                # update_results_DB
                   'ineligible INTEGER,'   		# __init__
-                  'encumbered INTEGER,' 			# N/A
+                  'encumbered INTEGER,' 		# N/A
+                  # Penalty???????
                   'car_number INTEGER,'			# __init__
                   'manufacturer TEXT,'			# __init__
-                  'sponsor TEXT'					# __init__
+                  'sponsor TEXT'				# __init__
                   ')')
         # init race database
         c.execute('CREATE TABLE IF NOT EXISTS Races ('
-		            'race_id INTEGER NOT NULL UNIQUE,'
-		            'series_id INTEGER,'
-		            'year INTEGER,'      # Do I need this if 'start_time' has the full date?
+		          'race_id INTEGER NOT NULL UNIQUE,'
+		          'series_id INTEGER,'
+		          'year INTEGER,'      # Do I need this if 'start_time' has the full date?
                   'start_time INTEGER,'
-		            'track_id INTEGER,'
+		          'track_id INTEGER,'
                   'race_name TEXT,'
                   'race_number INTEGER,'
                   'stage_length INTEGER,'
@@ -52,12 +53,18 @@ class Database:
                   'length REAL,'
                   'type TEXT'
                   ')')
+        # init drivers database
+        c.execute('CREATE TABLE IF NOT EXISTS Drivers ('
+                  'driver_id INTEGER NOT NULL UNIQUE, '
+                  'driver_name TEXT NOT NULL UNIQUE, '
+                  'PRIMARY KEY(driver_id)'
+                  ')')
         conn.commit()
         
         
-        
-# Now that this is in its own module, I think this should be its own method
-        
+    def add_results(self):
+        conn = sqlite3.connect('NASCAR.db')
+        c = conn.cursor()
         for driver in self.qry.driver_list:
             # Check if row already exists. If not, create it.
             c.execute('SELECT EXISTS(SELECT * FROM Race_Results WHERE driver_id=? AND race_id=?)', 
@@ -73,7 +80,7 @@ class Database:
                            driver['ineligible'], driver['car number'], 
                            driver['manufacturer'], driver['sponsor'])
                           )
-                conn.commit()
+        conn.commit()
 #        date_stamp = int(datetime.datetime(2018,2,11,12).timestamp())  
 #        c.execute('INSERT INTO Races(race_id, start_time) VALUES(?, ?)', (10, date_stamp))
 #        conn.commit()
@@ -82,13 +89,12 @@ class Database:
 #        print(dt[0])
 #        print(datetime.datetime.fromtimestamp(dt[0]).date())
 #        print(datetime.datetime.fromtimestamp(dt[0]).time())
-                
         print('Results DB initialized')
         c.close()
         conn.close()
         
         
-    def update_results_DB(self, stage):
+    def update_results(self, stage):
         stages = {
                 0: 'finish',
                 1: 'stage1',
@@ -105,20 +111,15 @@ class Database:
                 win = None
             c.execute('UPDATE Race_Results SET {}=?, laps_led=?, win=? WHERE driver_id=? AND race_id=?'.format(stages[stage]),
                       (driver['position'], driver['laps led'], win, driver['driver id'], self.qry.race_info['race id']))
-            conn.commit()
+        conn.commit()
         print('Results DB updated')
         c.close()
         conn.close()
         
         
-    def update_driver_DB(self):
+    def update_drivers(self):
         conn = sqlite3.connect('NASCAR.db')
         c = conn.cursor()
-        c.execute('CREATE TABLE IF NOT EXISTS Drivers ('
-                    'driver_id INTEGER NOT NULL UNIQUE, '
-                    'driver_name TEXT NOT NULL UNIQUE, '
-                    'PRIMARY KEY(driver_id)'
-                    ')')
         for driver in self.qry.driver_list:
             c.execute('SELECT EXISTS(SELECT driver_name FROM Drivers WHERE driver_id=?)',
                       (driver['driver id'],))
@@ -126,8 +127,72 @@ class Database:
             if data[0] == 0:
                 c.execute('INSERT INTO Drivers VALUES(?, ?)',
                           (driver['driver id'], driver['driver name']))
-                conn.commit()
                 print(f"{driver['driver name']} (ID = {driver['driver id']}) was added to the database")
+        conn.commit()
+                
         print('\nDatabase update complete')
+        c.close()
+        conn.close()
+        
+        
+    def add_race(self):
+        conn = sqlite3.connect('NASCAR.db')
+        c = conn.cursor()
+        c.execute('SELECT EXISTS(SELECT race_id FROM Races WHERE race_id=?)',
+                  self.qry.race_info['race id'])
+        data = c.fetchone()
+        if data[0] == 0:
+            c.execute('INSERT INTO Races(race_id, series_id, track_id, race_name, total_laps) VALUES(?, ?, ?, ?, ?)',
+                      (self.qry.race_info['race id'], self.qry.race_info['series id'],
+                       self.qry.race_info['track id'], self.qry.race_info['race name'],
+                       self.qry.race_status['total laps']))
+        else:
+            c.execute('UPDATE Races(series_id, track_id, race_name, total_laps) VALUES(?, ?, ?, ?)',
+                      (self.qry.race_info['series id'], self.qry.race_info['track id'],
+                       self.qry.race_info['race name'], self.qry.race_status['total laps']))
+        conn.commit()
+        c.close()
+        conn.close()
+        
+
+class live_race:
+        
+    def drop_table(self):
+        conn = sqlite3.connect('NASCAR.db')
+        c = conn.cursor()
+        c.execute('DROP TABLE IF EXISTS Live_Race')
+        conn.commit()
+        c.close()
+        conn.close()
+        
+    def add_table(self, driver_list):
+        self.drop_table()
+        conn = sqlite3.connect('NASCAR.db')
+        c = conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS Live_Race(driver_id INTEGER, "0" INTEGER)')
+        for driver in driver_list:
+            c.execute('INSERT INTO Live_Race VALUES(?, ?)',
+                      (driver['driver id'], driver['qual']))
+        conn.commit()
+        c.close()
+        conn.close()
+        
+        
+# Needs to accept 'race_status' to get lap info
+    def add_lap(self, driver_list, race_status):
+        conn = sqlite3.connect('NASCAR.db')
+        c = conn.cursor()
+        c.execute('PRAGMA table_info(Live_Race)')
+        col_list = c.fetchall()
+        lap = race_status['lap number']
+        lap_exists = False
+        for col in col_list:
+            if col[1] == lap:
+                lap_exists = True
+        if not lap_exists:
+            c.execute(f'ALTER TABLE Live_Race ADD COLUMN "{lap}" INTEGER')
+        for driver in driver_list:
+            c.execute(f'UPDATE Live_Race SET "{lap}"=? WHERE driver_id=?', (driver['position'], driver['driver id']))
+        conn.commit()
         c.close()
         conn.close()
