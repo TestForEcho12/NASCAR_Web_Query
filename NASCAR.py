2a5d3dc4cde888a9d5d27d209e1157bdd8dfa299
Greg <33642294+TestForEcho12@users.noreply.github.com>
2018-04-09 09:56:00 -0400

    Add files via upload

--- NASCAR.py
+++ NASCAR.py
@@ -3,30 +3,18 @@ import Database
 import excel
 import social
 import time
-import datetime
+import timer
 
 year = 2018
 series_id = 1
-race_id = 4680
-race_number = 5
-track = '@ACSupdates'
-hashtags = ['#AutoClub400',
-            '#NASCARGoesWest',]
+race_id = 4682
+race_number = 7
+stage_length = 85
+track = '@TXMotorSpeedway'
+hashtags = ['#OReilly500',]
 
 # pause until racetime
-now = datetime.datetime.now()
-future = datetime.datetime(2018,3,18,15,30)
-delta = future - now
-while delta.total_seconds() > 0:
-    hours = delta.seconds//3600
-    minutes = (delta.seconds//60)%60
-    seconds = delta.seconds - 3600*hours - 60*minutes
-    print(f'{hours}:{minutes}:{seconds} left until program start...')
-    time.sleep(60)
-    now = datetime.datetime.now()
-    delta = future - now
-print('\nPause complete. Starting program now!')
-
+timer.delay_start(2018,4,8,14,0)
 
 # set up live feed web object
 web = WebQuery3.WebData(year=year, series_id=series_id, race_id=race_id, feed_type=0)
@@ -41,11 +29,11 @@ fetch = Database.Fetch()
 db.web_query(web)
 db.update_drivers()
 db.update_tracks()
-db.add_race(year=year, race_number=race_number)
+db.add_race(year=year, race_number=race_number, stage_length=stage_length)
 db.add_results()
 
 # Qual results to Excel
-csv_col = '20'
+csv_col = '28'
 fetch.results_to_csv(race_id=race_id, stage_id=-1, col=csv_col)
 fetch.laps_to_csv(series=series_id, year=year)
 excel.results_from_csv(series=series_id)
@@ -64,8 +52,8 @@ reddit_id = reddit.get_id('Race Thread')
 
 # Stage 1
 stage = 1
-stage_lap = 60
-csv_col = '21'
+stage_lap = stage_length
+csv_col = '29'
 
 qry.live_race(stage_lap=stage_lap, refresh=3, results_pause=10)
 db.web_query(web)
@@ -83,15 +71,15 @@ excel.export_pictures(series=series_id)
 reddit = social.reddit()
 twitter = social.twitter(hashtags=hashtags)
 comment = social.imgur_upload(stage=stage)
-twitter.top_10(name_list=qry.qry.name_list, stage=stage)
+twitter.top_10(name_list=qry.qry.name_list, series_id=series_id, stage=stage)
 reddit.comment(url_id=reddit_id, comment=comment)
 twitter.standings(srs=series_id, stg=stage, track=track)
 
 
 # Stage 2
 stage = 2
-stage_lap = 120
-csv_col = '22'
+stage_lap = stage_length*2
+csv_col = '30'
 
 qry.live_race(stage_lap=stage_lap, refresh=3, results_pause=10)
 db.web_query(web)
@@ -104,7 +92,7 @@ excel.results_from_csv(series=series_id)
 
 print('Double checking Stage 1')
 stage = 1
-csv_col = '21'
+csv_col = '29'
 web = WebQuery3.WebData(year=year, series_id=series_id, race_id=race_id, feed_type=stage)
 db.web_query(web)
 db.update_results(stage=stage)
@@ -120,7 +108,7 @@ excel.export_pictures(series=series_id)
 reddit = social.reddit()
 twitter = social.twitter(hashtags=hashtags)
 comment = social.imgur_upload(stage=stage)
-twitter.top_10(name_list=qry.qry.name_list, stage=stage)
+twitter.top_10(name_list=qry.qry.name_list, series_id=series_id, stage=stage)
 reddit.comment(url_id=reddit_id, comment=comment)
 twitter.standings(srs=series_id, stg=stage, track=track)
 
@@ -129,7 +117,7 @@ twitter.standings(srs=series_id, stg=stage, track=track)
 # Finish
 stage = 0
 stage_lap = 0
-csv_col = '23'
+csv_col = '31'
 
 qry.live_race(stage_lap=stage_lap, refresh=3, results_pause=30)
 db.web_query(web)
@@ -142,7 +130,7 @@ excel.results_from_csv(series=series_id)
 
 print('Double checking Stage 2')
 stage = 2
-csv_col = '22'
+csv_col = '30'
 web = WebQuery3.WebData(year=year, series_id=series_id, race_id=race_id, feed_type=stage)
 db.web_query(web)
 db.update_results(stage=stage)
@@ -158,7 +146,7 @@ excel.export_pictures(series=series_id)
 reddit = social.reddit()
 twitter = social.twitter(hashtags=hashtags)
 comment = social.imgur_upload(stage=stage)
-twitter.top_10(name_list=qry.qry.name_list, stage=stage)
+twitter.top_10(name_list=qry.qry.name_list, series_id=series_id, stage=stage)
 reddit.comment(url_id=reddit_id, comment=comment)
 twitter.standings(srs=series_id, stg=stage, track=track)
 
@@ -166,6 +154,6 @@ twitter.standings(srs=series_id, stg=stage, track=track)
 # Post Race
 reddit_id = reddit.get_id('Post-Race')
 reddit.comment(url_id=reddit_id, comment=comment)
-time.sleep(45)
+time.sleep(60)
 reddit_id = reddit.get_id('Scorecard')
 reddit.comment(url_id=reddit_id, comment=comment)
