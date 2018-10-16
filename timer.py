9a8d573141fc99bb489e52421b66f3a51126ac9a
Greg <33642294+TestForEcho12@users.noreply.github.com>
2018-10-16 17:25:17 -0400

    Add files via upload

--- timer.py
+++ timer.py
@@ -22,3 +22,60 @@ def delay_start(year, month, day, hour, second):
         delta = future - now
         time_left = delta.total_seconds()
     print('\nPause complete. Starting program now!')
+    
+
+
+
+
+        
+        
+class delay_start2:
+    
+    def __init__(self, year, month, day, hour, second):
+        self.future = datetime.datetime(year, month, day, hour, second)
+        
+    def get_time_left(self):
+        now = datetime.datetime.now()
+        delta = self.future - now
+        self.time_left = delta.total_seconds() 
+    
+    def sleep(self, length):
+        while self.time_left > length:
+            print(f'Pausing {length} seconds... {int(self.time_left)} seconds remaining')
+            time.sleep(length)
+            self.time_left -= length
+            
+
+    
+def run(ds2):
+    
+    # 1 hour
+    ds2.get_time_left()
+    ds2.sleep(3600)
+    
+    # 10 minutes
+    ds2.get_time_left()
+    ds2.sleep(600)
+    
+    # 3 minutes
+    ds2.get_time_left()
+    ds2.sleep(180)
+    
+    # 30 seconds
+    ds2.get_time_left()
+    ds2.sleep(30)
+    
+    # 5 seconds
+    ds2.get_time_left()
+    ds2.sleep(5)
+
+    # 1 second
+    ds2.get_time_left()
+    ds2.sleep(1)
+    
+    print('Pause Complete')
+
+        
+    
+        
+        
