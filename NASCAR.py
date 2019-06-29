2a08e19bbfd1c2d27405bc5ae3b9a437228583cd
Greg <33642294+TestForEcho12@users.noreply.github.com>
2019-06-29 07:27:37 -0400

    Update NASCAR.py

--- NASCAR.py
+++ NASCAR.py
@@ -5,17 +5,17 @@ import social
 import timer
 
 year = 2019
-series_id = 1
-race_id = 4789
-race_number = 15
-stage_length = 60
-col = 60
-track = '@MISpeedway'
-hashtags = ['#FireKeepersCasino400', '#NASCAR']
+series_id = 3
+race_id = 4854
+race_number = 12
+stage_length = 35
+col = 46
+track = '@ChicagolndSpdwy'
+hashtags = ['#CampingWorld225', '#NASCAR']
 
 # pause until racetime
-timer.run(timer.delay_start2(2019,6,9,14,0))
-pause = 25
+timer.run(timer.delay_start2(2019,6,28,21,0))
+pause = 15
 
 
 # set up live feed web object
@@ -35,18 +35,20 @@ db.add_race(year=year, race_number=race_number, stage_length=stage_length)
 db.add_results()
 
 # Qual results to Excel
-exl = excel.Excel(year=year, series=series_id)
 csv_col = str(col)
 fetch.all_drivers(series=series_id, year=year)
-exl.all_drivers()
 fetch.ineligible_drivers(series=series_id, year=year)
-exl.ineligible_drivers()
 fetch.results_to_csv(race_id=race_id, stage_id=-1, col=csv_col)
-exl.results_from_csv()
-exl.calculate_points()
-exl.full_run()
 fetch.laps_to_csv(series=series_id, year=year)
-exl.laps_led()
+
+exl = excel.Excel(year=year, series=series_id)
+#exl.all_drivers()
+#exl.ineligible_drivers()
+#exl.results_from_csv()
+#exl.calculate_points()
+#exl.full_run()
+#exl.laps_led()
+exl.pre_race()
 
 # Set up live race position tracking
 live = Database.LiveRace()
@@ -70,11 +72,12 @@ db.update_laps()
 fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
 fetch.laps_to_csv(series=series_id, year=year)
 live.get_results()
-exl.results_from_csv()
 
-exl.calculate_points()
-exl.laps_led()
-exl.export_pictures()
+#exl.results_from_csv()
+#exl.calculate_points()
+#exl.laps_led()
+#exl.export_pictures()
+exl.in_race()
 
 twitter = social.twitter(series=series_id, track=track, hashtags=hashtags)
 twitter.top_10_standings(name_list=qry.qry.name_list, stg=stage)
@@ -86,16 +89,10 @@ reddit.comment(url_id=reddit_id, comment=comment)
 # Stage 2
 stage = 2
 stage_lap = stage_length*stage
-csv_col = str(col + stage)
-
 qry.live_race(stage_lap=stage_lap, refresh=3, results_pause=pause)
 db.web_query(web)
 db.update_results(stage=stage)
 db.update_laps()
-fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
-fetch.laps_to_csv(series=series_id, year=year)
-live.get_results()
-exl.results_from_csv()
 
 print('Double checking Stage 1')
 stage = 1
@@ -106,11 +103,18 @@ db.update_results(stage=stage)
 fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
 exl.results_from_csv()
 web = WebQuery3.WebData(year=year, series_id=series_id, race_id=race_id, feed_type=0)
-stage = 2
 
-exl.calculate_points()
-exl.laps_led()
-exl.export_pictures()
+stage = 2
+csv_col = str(col + stage)
+fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
+fetch.laps_to_csv(series=series_id, year=year)
+live.get_results()
+
+#exl.results_from_csv()
+#exl.calculate_points()
+#exl.laps_led()
+#exl.export_pictures()
+exl.in_race()
 
 twitter = social.twitter(series=series_id, track=track, hashtags=hashtags)
 twitter.top_10_standings(name_list=qry.qry.name_list, stg=stage)
@@ -122,16 +126,10 @@ reddit.comment(url_id=reddit_id, comment=comment)
 # Finish
 stage = 0
 stage_lap = 0
-csv_col = str(col + 3)
-
 qry.live_race(stage_lap=stage_lap, refresh=3, results_pause=pause*4)
 db.web_query(web)
 db.update_results(stage=stage)
 db.update_laps()
-fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
-fetch.laps_to_csv(series=series_id, year=year)
-live.get_results()
-exl.results_from_csv()
 
 print('Double checking Stage 2')
 stage = 2
@@ -142,11 +140,18 @@ db.update_results(stage=stage)
 fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
 exl.results_from_csv()
 web = WebQuery3.WebData(year=year, series_id=series_id, race_id=race_id, feed_type=0)
-stage = 0
 
-exl.calculate_points()
-exl.laps_led()
-exl.export_pictures()
+stage = 0
+csv_col = str(col + 3)
+fetch.results_to_csv(race_id=race_id, stage_id=stage, col=csv_col)
+fetch.laps_to_csv(series=series_id, year=year)
+live.get_results()
+
+#exl.results_from_csv()
+#exl.calculate_points()
+#exl.laps_led()
+#exl.export_pictures()
+exl.in_race()
 
 twitter = social.twitter(series=series_id, track=track, hashtags=hashtags)
 twitter.top_10_standings(name_list=qry.qry.name_list, stg=stage)
@@ -160,4 +165,3 @@ reddit_id = reddit.get_id(thread=2, series=series_id)
 reddit.comment(url_id=reddit_id, comment=comment)
 if series_id == 1:
     twitter.manufacturer()
-
